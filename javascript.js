6bca5f56f7535bfbde3e35fa8e2a88c51fd596b5
israelizzy123 <31420706+israelizzy123@users.noreply.github.com>
2019-01-29 14:48:04 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+// "el" stands for the "element" that was just clicked
+function opaque(el){
+	document.getElementById("one").classList.toggle("opaque");
+	
+
+}
+
+// "el" stands for the "element" that was just clicked
+function normal(el){
+	document.getElementById("two").classList.toggle("normal");
+	
+	document.getElementById("two").classList.toggle("normal");
+
+}
+function one(el){
+	document.getElementById("one").classList.toggle("7rings");
+	
+}	
+function two(el){
+	document.getElementById("two").classList.toggle("charles");
+	
+}	
+	
+function three(el){
+	document.getElementById("three").classList.toggle("arrow");		
+}
